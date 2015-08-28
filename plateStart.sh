2fee092100e8f2de40eec3ca2d801d87a10d01c4
root <root@raspberrypi.(none)>
2015-08-28 11:41:05 +0000

    Starting script and static folder

--- plateStart.sh
+++ plateStart.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo python /home/pi/Camera/FlaskServer/run.py & sudo python /home/pi/Camera/Camera/plate_with_server.py
+sudo python /home/pi/Camera/cam-raspi-tram/FlaskServer/run.py & 
+sudo python /home/pi/Camera/cam-raspi-tram/Camera/plate_with_server.py &
 
